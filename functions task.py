fd998286f50dd4175c9803e7caa368cda3af993c
ganesh909909 <victoryganesh143@gmail.com>
2020-08-17 08:21:41 +0530

    second commit

--- functions task.py
+++ functions task.py
@@ -1,16 +1,11 @@
-# l=range(1,101)
-# a=list(map(lambda x:x*5,l))
-# print(a)
-
 # k=range(1,1002)
 # a=list(filter(lambda x:x%13==0,[k for k in range(1,101)]))
 # print(a)
-
-
+#
+#
 # from functools import reduce as e
 # a=(e(lambda x,y:x*y,[k for k in range(1,101)]))
 # print((a))
-
 
 
 
