9fb3ad8a91f7168006938486230f91f6d5a7745f
ganesh909909 <victoryganesh143@gmail.com>
2020-08-17 08:46:58 +0530

    third commit

--- FFNRC.py
+++ FFNRC.py
@@ -1,13 +1,13 @@
-# def firstNotRepeatingCharacter(my_string):
-#     for i in range(len(my_string)):
-#         isRepeating=False
-#         for j in range(len(my_string)):
-#             if my_string[i]==my_string[j] and i!=j:
-#                 isRepeating=True
-#         if not isRepeating:
-#             return my_string[i]
-#     return  '_'
-# print(firstNotRepeatingCharacter('aabducdb'))
+def firstNotRepeatingCharacter(my_string):
+    for i in range(len(my_string)):
+        isRepeating=False
+        for j in range(len(my_string)):
+            if my_string[i]==my_string[j] and i!=j:
+                isRepeating=True
+        if not isRepeating:
+            return my_string[i]
+    return  '_'
+print(firstNotRepeatingCharacter('aabducdb'))
 
 
 def func(g):
